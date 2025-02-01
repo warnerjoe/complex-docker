2a8713244533d0d240d0e4eb15616322a963bbfe
thrash-particle <86530281+thrash-particle@users.noreply.github.com>
2025-01-31 22:08:59 -0600

    Fixed kubernetes

--- server/index.js
+++ server/index.js
@@ -11,12 +11,17 @@ app.use(bodyParser.json());
 
 // Postgres Client Setup
 const { Pool } = require("pg");
+
 const pgClient = new Pool({
   user: keys.pgUser,
   host: keys.pgHost,
   database: keys.pgDatabase,
   password: keys.pgPassword,
   port: keys.pgPort,
+  ssl:
+    process.env.NODE_ENV !== 'production'
+      ? false
+      : { rejectUnauthorized: false },
 });
 
 pgClient.on("connect", (client) => {
